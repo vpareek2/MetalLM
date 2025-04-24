8b5bb9e6f68275cbc84e93d19af8259bc910fdd3
Veer Pareek <veerpareek12@gmail.com>
2025-04-24 15:46:23 -0500

    Initial commit of MetaLLM project structure

---
+++ MetalLM/MetalLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLMApp.swift
+//  MetalLM
+//
+//  Created by Veer Pareek on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetalLMTests/MetalLMTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLMTests.swift
+//  MetalLMTests
+//
+//  Created by Veer Pareek on 4/24/25.
+//
+
+import Testing
+@testable import MetalLM
+
+struct MetalLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
